d8aa65ce3c3aab3de59fd7a362cf2679c153a803
HindAlmorshed <81118373+HindAlmorshed@users.noreply.github.com>
2024-05-07 11:26:29 +0200

    w

--- resources/views/employee/orders/order_to_work.blade.php
+++ resources/views/employee/orders/order_to_work.blade.php
@@ -60,6 +60,9 @@
 												<th class="wd-15p border-bottom-0">الموقع</th>
 												<th class="wd-15p border-bottom-0">طريقة الدفع</th>
 
+												<th class="wd-15p border-bottom-0">الخدمة الأساسية</th>
+												<th class="wd-15p border-bottom-0">الخدمات الإضافية</th>
+
 												<th class="wd-15p border-bottom-0">نوع السيارة</th>
 												<th class="wd-15p border-bottom-0">حجم السيارة</th>
 												<th class="wd-15p border-bottom-0">رقم السيارة</th>
@@ -97,6 +100,30 @@
 												<td> </td>
 												@endif
 
+
+												@if(isset($results) && is_array($results) && !empty($results))
+												@if($results->type == 'أساسية')
+												
+													<td>{{ $results->name }}</td>
+
+												@endif
+												@endif
+
+
+												@if(isset($results) && is_array($results) && !empty($results))
+												@if($results->type == 'إضافية')
+
+												@foreach($results as $result)
+													@if($result)
+												
+													<td>{{ $results->name }}</td>
+
+													@endif
+												@endforeach
+												@endif
+											@endif
+
+
 												<td>{{$order->typeOfCar}}</td>
 												<td>{{$order->sizeOfCar}}</td>
 												<td>{{$order->numOfCar}}</td>
@@ -116,43 +143,10 @@
 													<form action="{{ route('ord.chooseEmp', $order->id ) }}" method="POST" enctype="multipart/form-data" autocomplete="off">
 														@csrf
 														@method('POST')
-														<button class="btn btn-sm btn-success" title="اختر الموظف"><i class="fas fa-user"></i></button>
+														<button class="btn btn-sm btn-success" title="إنهاء الطلب"><i class="fas fa-user"></i></button>
 													</form>
 												</td>
 
-												<td>
-													<a class="modal-effect btn btn-sm btn-danger" data-toggle="modal" title="رفض" style="cursor: pointer;"
-													data-target="#delete"><i class="fas fa-times"></i></a>
-													<form action="{{route('ord.updatePenddingToCanceled', $order->id)}}" method="POST" enctype="multipart/form-data">
-														@csrf
-														@method('POST')
-														<div id="delete" class="modal fade delete-modal" role="dialog">
-															<div class="modal-dialog modal-dialog-centered">
-																<div class="modal-content">
-			
-																	<div class="modal-header">
-																		<h6 class="modal-title">سبب رفض طلب : &nbsp; {{ App\Models\User::findOrFail($order->user_id)->name }} </h6><button aria-label="Close" class="close" data-dismiss="modal"
-																			type="button"><span aria-hidden="true">&times;</span></button>
-																	</div>
-			
-																	<div class="modal-body text-center">
-																		<h5>سبب الرفض</h5>
-																		<br>
-																		<input type="text" class="form-control" id="inputName" name="note">
-																		{{-- <img src="{{URL::asset('assets/img/media/sent.png')}}" alt="" width="50" height="46"> --}}
-																		<br>
-																		{{-- <h5>هل أنت متأكد من عملية الحذف؟</h5> --}}
-																		<br>
-																		<div class="m-t-20"> <a href="#" class="btn btn-white" data-dismiss="modal">إلغاء</a>
-																			<button type="submit" class="btn btn-success">إرسال</button>
-																		</div>
-																		<br>
-																	</div>
-																</div>
-															</div>
-														</div>
-													</form>
-												</td>
 											</tr>
 											@endforeach
 										</tbody>
