cd1fe9e302aeee3f3cdb6d1e091317f04a31f0d4
HindAlmorshed <81118373+HindAlmorshed@users.noreply.github.com>
2024-05-13 11:39:26 +0200

    settings

--- resources/views/admin/employees/show_count.blade.php
+++ resources/views/admin/employees/show_count.blade.php
@@ -6,7 +6,7 @@
 				<div class="breadcrumb-header justify-content-between">
 					<div class="my-auto">
 						<div class="d-flex">
-							<h4 class="content-title mb-0 my-auto">أعمال الموظف</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">{{ $emp_name->firstName }} {{ $emp_name->lastName }}</span>
+							<h4 class="content-title mb-0 my-auto">أعمال الموظف</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0"> {{ $emp_name->firstName }} {{ $emp_name->lastName }}</span>
 						</div>
 					</div>
 				</div>
